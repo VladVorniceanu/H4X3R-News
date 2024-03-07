5856af7035ef4028338c42be6b600b7fd818d91b
VladVorniceanu <vorniceanu.v@gmail.com>
2024-03-07 15:09:12 +0200

    Initial Commit

---
+++ H4X3R-News/H4X3R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X3R_NewsApp.swift
+//  H4X3R-News
+//
+//  Created by Vlad Vorniceanu on 07.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct H4X3R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
